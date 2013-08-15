e644e2b34b6b6a0220b6a378c81b928f31263a6b
Barbara Bothe <barbara.bothe@fau.de>
2013-08-15 13:25:04 +0200

    Interne Links relativ ausgeben

--- functions.php
+++ functions.php
@@ -1049,3 +1049,34 @@ class My_Walker_Nav_Menu extends Walker_Nav_Menu {
         parent::display_element($el, $children, $max_depth, $depth, $args, $output);
     }
 }
+
+
+/* Interne Links relativ ausgeben */
+
+add_action( 'template_redirect', 'rw_relative_urls' );
+    function rw_relative_urls() {
+    // Don't do anything if:
+    // - In feed
+    // - In sitemap by WordPress SEO plugin
+    if ( is_feed() || get_query_var( 'sitemap' ) )
+    return;
+    $filters = array(
+    'post_link',
+    'post_type_link',
+    'page_link',
+    'attachment_link',
+    'get_shortlink',
+    'post_type_archive_link',
+    'get_pagenum_link',
+    'get_comments_pagenum_link',
+    'term_link',
+    'search_link',
+    'day_link',
+    'month_link',
+    'year_link',
+    );
+    foreach ( $filters as $filter )
+    {
+    add_filter( $filter, 'wp_make_link_relative' );
+    }
+    }
