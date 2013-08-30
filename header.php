d3a66700ebe0b677af8afccfcee191674e83f1b0
Rolf v.d. Forst <rolf.v.d.forst@googlemail.com>
2013-08-30 16:21:29 +0200

    favicon hinzugefügt

--- header.php
+++ header.php
@@ -12,6 +12,7 @@ global $options;
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/grafiken/favicon.ico" />
     <?php wp_head(); ?>
 </head>
 
